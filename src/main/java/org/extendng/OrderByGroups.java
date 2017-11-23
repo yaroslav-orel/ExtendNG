c499d1a31032db72cfdfb62cf630bc60188fb46a
yaroslav-orel <yaroslav.orel.kh@gmail.com>
2017-11-23 14:59:05 +0200

    Remove unused imports

--- src/main/java/org/extendng/OrderByGroups.java
+++ src/main/java/org/extendng/OrderByGroups.java
@@ -1,14 +1,11 @@
 package org.extendng;
 
-import com.google.common.collect.Streams;
 import org.testng.IMethodInstance;
 import org.testng.IMethodInterceptor;
 import org.testng.ITestContext;
 
-import java.lang.reflect.Method;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.stream.Stream;
