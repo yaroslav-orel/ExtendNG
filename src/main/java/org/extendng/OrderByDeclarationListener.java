fc7eaa1410aaa4b769d09b66663db21457441a4f
yaroslav-orel <yaroslav.orel.kh@gmail.com>
2018-04-04 18:32:11 +0300

    Refactor OrderByDeclarationListener

--- src/main/java/org/extendng/OrderByDeclarationListener.java
+++ src/main/java/org/extendng/OrderByDeclarationListener.java
@@ -1,47 +1,50 @@
 package org.extendng;
 
 import kiss.util.Reflect;
+import lombok.val;
 import org.testng.IMethodInstance;
 import org.testng.IMethodInterceptor;
 import org.testng.ITestContext;
 
 import java.lang.reflect.Method;
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
+import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 import static org.extendng.ReflectionUtils.shouldBeInvoked;
 
 public class OrderByDeclarationListener implements IMethodInterceptor {
+
     @Override
     public List<IMethodInstance> intercept(List<IMethodInstance> methods, ITestContext context) {
-        return methods.stream()
-                .collect(groupingBy(IMethodInstance::getInstance))
-                .entrySet().stream()
-                    .flatMap(e ->
-                        shouldBeInvoked(e.getKey().getClass(), OrderByDeclarationListener.class) ?
-                                e.getValue().stream().sorted(byDeclaration()) :
-                                e.getValue().stream())
-                .collect(toList());
+        val testClassesMap = groupTestsByTestClasses(methods);
+        orderTestsByDeclarationInsideTestClassesWithListener(testClassesMap);
+
+        return flattenTestClasses(testClassesMap);
     }
 
-    public Comparator<IMethodInstance> byDeclaration(){
-        return new Comparator<IMethodInstance>() {
+    private Map<Object, List<IMethodInstance>> groupTestsByTestClasses(List<IMethodInstance> methods){
+        return methods.stream().collect(groupingBy(method -> method.getInstance(), LinkedHashMap::new, toList()));
+    }
 
-            List<Method> classMethodsInOrder;
+    private List<IMethodInstance> flattenTestClasses(Map<Object, List<IMethodInstance>> testClassesMap) {
+        return testClassesMap.entrySet().stream().flatMap(e -> e.getValue().stream()).collect(toList());
+    }
 
-            @Override
-            public int compare(IMethodInstance o1, IMethodInstance o2) {
-               if(classMethodsInOrder == null){
-                   classMethodsInOrder = Stream.of(Reflect.getDeclaredMethodsInOrder(o1.getInstance().getClass())).collect(toList());
-               }
-                return Integer.compare(
-                        classMethodsInOrder.indexOf(o1.getMethod().getConstructorOrMethod().getMethod()),
-                        classMethodsInOrder.indexOf((o2.getMethod().getConstructorOrMethod().getMethod()))
-                );
-            }
-        };
+    private void orderTestsByDeclarationInsideTestClassesWithListener(Map<Object,List<IMethodInstance>> testClassesMap) {
+        testClassesMap.entrySet().stream()
+                .filter(e -> shouldBeInvoked(e.getKey().getClass(), OrderByDeclarationListener.class))
+                .forEach(entry -> entry.getValue().sort(
+                        byDeclaration(Stream.of(Reflect.getDeclaredMethodsInOrder(entry.getKey().getClass())).collect(toList()))
+                ));
+    }
+
+    private Comparator<IMethodInstance> byDeclaration(List<Method> classMethodsInOrder){
+        return comparingInt(o -> classMethodsInOrder.indexOf(o.getMethod().getConstructorOrMethod().getMethod()));
     }
 }
