d7c820fc7b4b3036e4b9b6ecacb171ee762393a0
yaroslav-orel <yaroslav.orel.kh@gmail.com>
2018-04-06 23:45:11 +0300

    Cover special cases in OrderByGroupsListener

--- src/main/java/org/extendng/OrderByGroupsListener.java
+++ src/main/java/org/extendng/OrderByGroupsListener.java
@@ -11,6 +11,7 @@ import java.util.stream.Stream;
 
 import static java.util.Comparator.comparingInt;
 import static org.extendng.MethodInterceptorUtils.filterAndSortTests;
+import static org.extendng.ReflectionUtils.invokeMethod;
 import static org.extendng.ReflectionUtils.shouldBeInvoked;
 
 public class OrderByGroupsListener implements IMethodInterceptor {
@@ -23,30 +24,39 @@ public class OrderByGroupsListener implements IMethodInterceptor {
     }
 
     public static void sortByGroupsOrByGroupOrder(Map.Entry<Object, List<IMethodInstance>> entry){
-        entry.getValue().sort(byGroupsOrByGroupOrder(entry.getKey()));
+        entry.getValue().sort(byByGroupOrderOrByGroupHash(entry.getKey()));
     }
 
-    public static Comparator<IMethodInstance> byGroupsOrByGroupOrder(Object testClassInstance){
+    public static Comparator<IMethodInstance> byByGroupOrderOrByGroupHash(Object testClassInstance){
         val groupOrder = getGroupOrder(testClassInstance);
 
         return groupOrder.isPresent() ?
-                comparingInt(method -> getGroupIndex(method, groupOrder.get())):
-                comparingInt(method -> Arrays.hashCode(method.getMethod().getGroups()));
+                comparingByGroupOrder(groupOrder.get()).thenComparingInt(OrderByGroupsListener::byGroupHash):
+                comparingInt(OrderByGroupsListener::byGroupHash);
     }
 
+    public static Comparator<IMethodInstance> comparingByGroupOrder(List<String> groupOrder){
+        return comparingInt(method -> getGroupIndex(method, groupOrder));
+    }
+
+    public static int byGroupHash(IMethodInstance method){
+        return Arrays.hashCode(method.getMethod().getGroups());
+    }
+
+    @SuppressWarnings("unchecked")
     private static Optional<List<String>> getGroupOrder(Object testClassInstance){
         return Stream.of(testClassInstance.getClass().getDeclaredMethods())
                 .filter(method -> method.isAnnotationPresent(GroupOrder.class))
                 .findFirst()
-                .map(method -> (List<String>) ReflectionUtils.invokeMethod(method, testClassInstance));
+                .map(method -> (List<String>) invokeMethod(method, testClassInstance));
     }
 
     private static int getGroupIndex(IMethodInstance method, List<String> order) {
         return order.stream()
                 .filter(item -> ArrayUtils.contains(method.getMethod().getGroups(), item))
                 .findFirst()
-                .map(item -> order.indexOf(item))
-                .get();
+                .map(order::indexOf)
+                .orElse(-1);
     }
 
     private static boolean shouldListen(Map.Entry<Object, List<IMethodInstance>> testClass){
